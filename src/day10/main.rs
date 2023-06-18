1e0f552504eb67d557db72808a77bf9a436a05df
Wendell Smith <wackywendell@gmail.com>
2023-06-18 19:03:52 -0400

    Day 10 Part 1

--- src/day10/main.rs
+++ src/day10/main.rs
@@ -1,11 +1,84 @@
-use std::fs::File;
-use std::io::BufReader;
 use std::path::PathBuf;
 
 use clap::Parser;
 use log::debug;
 
-use adventofcode2021::parse;
+pub fn pair(c: char) -> Option<char> {
+    match c {
+        '(' => Some(')'),
+        '<' => Some('>'),
+        '{' => Some('}'),
+        '[' => Some(']'),
+        ')' => Some('('),
+        '>' => Some('<'),
+        '}' => Some('{'),
+        ']' => Some('['),
+        _ => None,
+    }
+}
+
+pub fn mismatches(s: &str) -> (Vec<char>, Vec<char>) {
+    let mut closers = Vec::new();
+    let mut stack = Vec::new();
+    for c in s.chars() {
+        match c {
+            '(' | '<' | '{' | '[' => stack.push(c),
+            ')' | '>' | '}' | ']' => {
+                let expected = pair(c).unwrap();
+                let popped = stack.pop();
+                match popped {
+                    None => {
+                        closers.push(c);
+                    }
+                    Some(p) if p == expected => {
+                        // It matches, all is well
+                    }
+                    Some(p) => {
+                        // It doesn't match; put it in the list of closers,
+                        // and put the popped one back on
+                        stack.push(p);
+                        closers.push(c);
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    (stack, closers)
+}
+
+pub fn score_mismatches(s: &str) -> i64 {
+    let mut score = 0;
+
+    for line in s.lines() {
+        let t = line.trim();
+        if t.is_empty() {
+            continue;
+        }
+
+        let (_unclosed, closers) = mismatches(t);
+
+        // debug!(
+        //     "Line: {t}\n    {unclosed:?}, {closers:?}",
+        //     unclosed = _unclosed.iter().collect::<String>(),
+        //     closers = closers.iter().collect::<String>(),
+        // );
+
+        let s = match closers.first() {
+            None => continue,
+            Some(')') => 3,
+            Some(']') => 57,
+            Some('}') => 1197,
+            Some('>') => 25137,
+            Some(c) => panic!("Unexpected closer {c}"),
+        };
+
+        score += s;
+    }
+
+    score
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Main
@@ -22,11 +95,11 @@ fn main() {
     let args = Args::parse();
 
     debug!("Using input {}", args.input.display());
-    let file = File::open(args.input).unwrap();
-    let buf = BufReader::new(file);
-    let nums: Vec<i64> = parse::buffer(buf).unwrap();
+    let s = std::fs::read_to_string(&args.input).unwrap();
 
-    println!("Found {length} lines: {nums:?}", length = nums.len());
+    let score = score_mismatches(&s);
+
+    println!("Found score {score}");
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -40,16 +113,21 @@ mod tests {
     use super::*;
 
     const EXAMPLE: &str = r###"
-        2
-        3
-        5
-        7
-        9
+        [({(<(())[]>[[{[]{<()<>>
+        [(()[<>])]({[<{<<[]>>(
+        {([(<{}[<>[]}>{[]{[(<()>
+        (((({<>}<{<{<>}{[]{[]{}
+        [[<[([]))<([[{}[[()]]]
+        [{[{({}]{}}([{[{{{}}([]
+        {<[[]]>}<{[{[{[]{()[[[]
+        [<(<(<(<{}))><([]([]()
+        <{([([[(<>()){}]>(<<{{
+        <{([{{}}[<[[[<>{}]]]>[]]
     "###;
 
     #[test]
     fn test_basic() {
-        let nums: Vec<i64> = parse::buffer(EXAMPLE.as_bytes()).unwrap();
-        assert_eq!(nums, vec![2, 3, 5, 7, 9]);
+        let score = score_mismatches(EXAMPLE);
+        assert_eq!(score, 26397);
     }
 }
